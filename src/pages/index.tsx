d6ba2311f7b0bcba631d482cabdd916e894c4b01
MustyMouse <admin@krux.us>
2021-08-25 13:19:19 -0500

    Changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Page() {
 
       <main className="content">
         <Hero
-          title="Get Started with Headless"
+          title="Get Started with Atlas"
           buttonText="Developer Docs"
           buttonURL="https://developers.wpengine.com/"
           button2Text="Headless on GitHub"
