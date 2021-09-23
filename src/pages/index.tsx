7c39301e894ea21fb1746ce0f32a920f895d694f
MustyMouse <admin@krux.us>
2021-09-22 19:57:26 -0500

    Oops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { getNextStaticProps } from '@faustjs/next';
 
 import { GetStaticPropsContext } from 'next';
-import Head from 'next/head'; broken_code
+import Head from 'next/head';
 import React from 'react';
 import { CTA, Footer, Header, Hero, Posts } from 'components';
 import styles from 'scss/pages/home.module.scss';
